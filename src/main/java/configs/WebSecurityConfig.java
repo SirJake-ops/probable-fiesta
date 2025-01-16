815b86605275cb18d34ff8af21469f41652c3b5a
Jacob Pagan <jacob.pagan10@gmail.com>
2025-01-16 13:26:46 -0600

    Added the security filter chain for the auth

--- src/main/java/configs/WebSecurityConfig.java
+++ src/main/java/configs/WebSecurityConfig.java
@@ -1,8 +1,36 @@
 package configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(a -> a.requestMatchers("/", "/error", "/webjars/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout").logoutSuccessUrl("/").permitAll()
+                );
+
+
+        return httpSecurity.build();
+    }
 
 }
