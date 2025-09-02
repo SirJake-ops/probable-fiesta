70ce36c5e2a3d6e15a5b94de808c5b73227a6c27
Jacob Pagan <jacob.pagan10@gmail.com>
2025-09-02 15:49:49 -0500

    [any message]

--- src/main/java/AuctionUser/domain/exceptions/PropertyInvalidException.java
+++ src/main/java/AuctionUser/domain/exceptions/PropertyInvalidException.java
@@ -1,6 +1,6 @@
 package AuctionUser.domain.exceptions;
 
-public class PropertyInvalidException extends Exception {
+public class PropertyInvalidException extends RuntimeException {
     public PropertyInvalidException(String message){
         super(message);
     }
